fb6d37513314f8365352e24dcfd878abc34d9dc7
Mayank Sharma <mayankece155@gmail.com>
2021-06-20 18:23:03 +0530

    make it better

--- app.js
+++ app.js
@@ -51,11 +51,13 @@ app.use((error, req, res, next) => {
 });
 
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  })
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
 }
 
 mongoose
