40fdc48109a6c84e4c4408e52671896e55f30275
Mayank Sharma <mayankece155@gmail.com>
2026-05-29 10:48:00 +0530

    dockerized place tracker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14
+FROM node:14-alpine
 
 WORKDIR /app
 
@@ -10,4 +10,4 @@ COPY . .
 
 EXPOSE 5000
 
-CMD ["node", "app.js"]
+CMD ["npm", "start"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,56 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "mern_app" {
-  ami           = "ami-03f4878755434977f"
-  instance_type = "t3.micro"
+resource "aws_security_group" "app_sg" {
+  name = "place-tracker-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                    = "ami-03bb6d83c60fc5f7c"
+  instance_type          = "t3.micro"
+  key_name               = "place-tracker-app-key"
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
-    Name = "terraform-demo"
+    Name = "place-tracker-server"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.app_server.public_ip
 }
