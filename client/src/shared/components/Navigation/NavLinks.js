f15c2486b64e0bea61db859c22b12bd627a95e72
Mayank Sharma <mayankece155@gmail.com>
2026-05-29 15:36:50 +0530

    Navlink rollback

--- client/src/shared/components/Navigation/NavLinks.js
+++ client/src/shared/components/Navigation/NavLinks.js
@@ -10,7 +10,7 @@ const NavLinks = (props) => {
     <ul className="nav-links">
       <li>
         <NavLink to="/" exact>
-          ALL USERS LISTS
+          ALL USERS
         </NavLink>
       </li>
       {auth.isLoggedIn && (
